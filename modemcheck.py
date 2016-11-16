591c3bb8a6ac9b199007e66ae25fa4a6f87ef043
Andrew Taylor <snowsquizy@users.noreply.github.com>
2016-11-16 17:13:42 +1100

    Create modemcheck.py
    Initial version tested with DLink DSL2870B ADSL Modem

---
+++ modemcheck.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python  
+########################################################
+# Confirm internet connection.  If internet is down reboot adsl modem
+# Andrew Taylor @ 2016# Version 0.1
+######################################################## 
+
+########################################################
+## Libraries Imports
+# Regular expressions
+import re
+# Subprocesses
+import subprocess
+# SSH Connection
+import paramiko
+
+########################################################
+## Variables
+# ADSL Modem IP Address
+modem = "192.168.1.1"
+# Ping Target
+host = "www.google.com.au"
+# ADSL Modem User
+user = "XXXX"
+# ADSL Modem Password
+m_password = "XXXX"
+# Expression for packet loss 
+noconnection = "100% packet loss"
+# ADSL Modem Reboot Command
+command_run = "reboot"
+
+########################################################
+# Confirm Packet connection to external website
+ping = subprocess.Popen(
+  ["ping", "-c", "2", host],    
+  stdout = subprocess.PIPE,    
+  stderr = subprocess.PIPE
+  )
+out, error = ping.communicate()
+
+########################################################
+# Connect to ADSL Modem and Reboot
+if re.search(noconnection,out,flags=0):        
+  # Create SSH Connection        
+  client = paramiko.SSHClient()        
+  # Load System Host Keys         
+  client.load_system_host_keys()        
+  # If connection is not in Host Keys add it        
+  client.set_missing_host_key_policy(paramiko.AutoAddPolicy())        
+  # Open SSH Connection to Modem        
+  client.connect(modem,username=user,password=m_password)        
+  # Run Reboot command        
+  client.exec_command(command_run)        
+  # Close SSH Connection        
+  client.close()
